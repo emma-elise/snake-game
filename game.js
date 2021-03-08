87d92a1a4becb186c05f89a11558434e56811220
loirratse <70772019+loirratse@users.noreply.github.com>
2021-03-07 20:03:02 -0500

    Update game.js
    Took game speed from rules.js to update correctly.

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 import { updateSnake } from "./snake.js";
 import { updateFood } from "./food.js";
-import { gameSpeed } from "./rules.js";
+import { gameScore } from "./rules.js";
+
+let gameSpeed = 250; // default speed of game for setInterval
 
 function createGrid() {
   for (var rows = 0; rows < 21; rows++) {
@@ -11,6 +13,17 @@ function createGrid() {
   $(".grid").width(840 / 21);
   $(".grid").height(840 / 21);
 }
+
+// gameSpeed increases per gameScore; setInterval =/= real time due to game lag, so it's more there to simply speed up
+if (gameScore >= 5 && gameScore <= 9) {
+  gameSpeed = 200;
+} else if (gameScore >= 10 && gameScore <= 19) {
+  gameSpeed = 125;
+} else if (gameScore >= 20 && gameScore <= 29) {
+  gameSpeed = 50;
+} else if (gameScore >= 30 && gameScore <= 39) {
+  gameSpeed = 25;
+} else if (gameScore >= 40) gameSpeed = 15;
 
 createGrid();
 
